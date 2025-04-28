8724fb8a1d966f331d6c93079404d4514f451a11
Lorenzo Radice <l.21radice@gmail.com>
2025-04-28 19:39:00 +0200

    Add populate_tables.sql to seed initial data for all tables
    - Created a new SQL script to populate the database with example data.
    - Included references to individual example data scripts for administrators, artisans, clients, products, and orders.

---
+++ db/populate_tables.sql
@@ -0,0 +1,7 @@
+\i './create_tables.sql';
+-- Populate all tables in the database
+\i './examples/ex_amministratori.sql';
+\i './examples/ex_artigiani.sql';
+\i './examples/ex_clienti.sql';
+\i './examples/ex_prodotti.sql';
+\i './examples/ex_ordini.sql';
