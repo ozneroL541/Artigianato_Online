93c2cc492602b374a87351fe46145c1a191a2fed
Lorenzo Radice <l.21radice@gmail.com>
2025-04-28 17:50:59 +0200

    Add comment to clarify the purpose of the database creation script

--- db/create_db.sql
+++ db/create_db.sql
@@ -1,3 +1,4 @@
+-- Script to create the user and database for the project
 CREATE USER artigianato_online WITH ENCRYPTED PASSWORD 'password' CREATEDB;
 CREATE DATABASE artigianato_online_db
     WITH 
