f82459ba847ef2278c79dcffd5836d7af65ea532
Sballerini3 <ballerinistefano81@gmail.com>
2025-05-01 11:19:00 +0200

    Ho creato la base per alcune funzioni

--- backend/cliente.js
+++ backend/cliente.js
@@ -12,3 +12,43 @@ const Client = new client({
 Client.connect();
 
 function query_test(){}
+
+
+async function loginAmm(){
+     
+ }
+
+async function logoutAmm(){
+
+}
+
+async function RegArtigiano(){
+
+}
+
+async function CaricaProd(){
+
+}
+
+async function loginArt(){
+     
+}
+
+async function logoutArt(){
+
+}
+
+
+async function RegCliente(){
+
+}
+
+async function loginCliente(){
+
+}
+
+async function logoutCliente(){
+
+}
+
+
