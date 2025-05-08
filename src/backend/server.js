9036173490ddfca36fcf6c409e2d9e27d88654e3
Lorenzo Radice <l.21radice@gmail.com>
2025-05-08 20:37:51 +0200

    Refactor frontend structure: remove old HTML files, add new pages for About, Artigiano Dashboard, and Login/Register functionalities; implement custom web components for layout and user messages; enhance styling with CSS variables and responsive design.

--- src/backend/server.js
+++ src/backend/server.js
@@ -2,13 +2,9 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 8080;
-const frontendPath = path.join(__dirname, '../frontend');
+const frontendPath = path.join(__dirname, '../frontend/public');
 
 app.use(express.static(frontendPath));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(frontendPath, 'pages/index.html'));
-});
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
