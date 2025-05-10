4bb8c8010987da49dc81ef1096bdb778e8e091e9
Giaki03 <79930388+Giaki03@users.noreply.github.com>
2025-05-10 22:03:25 +0200

    test?

--- src/backend/connessioneDB.js
+++ src/backend/connessioneDB.js
@@ -23,9 +23,9 @@ app.listen(5000, () => console.log("PostgreSQL API running on http://localhost:5
 
 
 //const express = require('express');
-
-import pg from 'pg'
-const { Pool, Client } = pg
+/*
+import pg from 'pg';
+const { Pool, Client } = pg;
  
 const pool = new Pool({
     host: "localhost",
@@ -33,8 +33,30 @@ const pool = new Pool({
     user: "artigianato_online",
     password: "password",
     database: "artigianato_online_db"
-})
+});
  
-console.log(await pool.query('SELECT NOW()'))
- 
-await client.end()
+console.log(await pool.query('SELECT * FROM clienti'));
+*/
+
+require('dotenv').config();
+const express = require('express');
+const { Pool } = require('pg');
+const cors = require('cors');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const pool = new Pool({ connectionString: process.env.PG_URI });
+
+// GET all users
+app.get('/users', async (req, res) => {
+    //const { rows } = await pool.query('SELECT * FROM users');
+    //res.json(rows);
+    console.log('culo');
+});
+
+app.listen(5000, () => console.log("PostgreSQL API running on http://localhost:5000"));
+
+
+
