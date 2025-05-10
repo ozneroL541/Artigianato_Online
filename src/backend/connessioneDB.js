2aed059a678c3adbaed14ff6cffccb34bdbd8198
Giaki03 <79930388+Giaki03@users.noreply.github.com>
2025-05-10 18:46:55 +0200

    test connessione al database

--- src/backend/connessioneDB.js
+++ src/backend/connessioneDB.js
@@ -1,4 +1,21 @@
-const {Pool} = require("pg");
+/*
+require('dotenv').config();
+const express = require('express');
+const {Pool} = require('pg');
+const cors = require('cors');
 
 
-const pool = new Pool({connectionString: process.env.PG_URL});
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const pool = new Pool({connectionString: process.env.PG_URI});
+
+// GET all users
+app.get('/users', async (req, res) => {
+    const { rows } = await pool.query('SELECT * FROM users');
+    res.json(rows);
+});
+
+app.listen(5000, () => console.log("PostgreSQL API running on http://localhost:5000"));
+*/
