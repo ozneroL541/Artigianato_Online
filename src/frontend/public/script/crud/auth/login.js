f88d4342a8f90f6179049caa86a83916b4691b7f
leonardobasso <parliamodiarte1@gmail.com>
2025-05-19 11:57:05 +0200

    File restructuring and implementing use of localstorage. Login function simplified

---
+++ src/frontend/public/script/crud/auth/login.js
@@ -0,0 +1,31 @@
+import {setUserInfo} from '../../state.js';
+
+/**
+ * This function implements the API for the artisan login
+ * @param {string} username the username of the user
+ * @param {string} password The user password
+ * @param {string} type If the user is an artisan or a client
+ * @author Leonardo Basso
+ */
+export const login = async (username, password, type) => {
+    try {
+        const response = await fetch(`http://localhost:8080/api/auth/login/${type}`, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username, password}),
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            setUserInfo(data.token, type);
+            window.location.href = '/';
+        } else {
+            alert(data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('An error occurred during login.');
+    }
+};
--- src/frontend/public/script/state.js
+++ src/frontend/public/script/state.js
@@ -1,24 +1,28 @@
 /**
- * This object contains the current state of the program. It has values about: `user info`
+ * This function saves the jwt token, the type of the user and a variable that indicates that the user is logged inside
+ * the browser's local storage
+ * @param jwt {string} The user Token
+ * @param userType {string} If the user is a client or an artisan
+ * @author Leonardo Basso
  * */
-const state = {
-    user: {
-        isLogged: false,
-        jwt_Token: null,
-        userType: null, // [Artigiano | Client]
-        username: null,
+export function setUserInfo(jwt, userType) {
+    localStorage.setItem('userToken', jwt)
+    localStorage.setItem('userType', userType)
+    localStorage.setItem('isUserLogged', "true")
+}
+
+/**
+ * This function returns an array with the user infos
+ * @return {object} an object with the following parameters: <br>
+ * `userToken` for the JWT Token <br>
+ * `userType` for the user Type, which should always be artisan or client <br>
+ * `isUserLogged` for checking if the user is logged or not
+ * @author Leonardo Basso
+ * */
+export function getUserInfo() {
+    return {
+        userToken: localStorage.getItem("userToken"),
+        userType: localStorage.getItem("UserType"),
+        isUserLogged: localStorage.getItem("isUserLogged")
     }
-};
-
-const listeners = [];
-
-const updateUserInfo = (newState) => {
-    Object.assign(state.user, newState);
-    listeners.forEach(listener => listener(state));
-};
-
-const subscribe = (listener) => {
-    listeners.push(listener);
-};
-
-export { state, updateUserInfo, subscribe };
+}
