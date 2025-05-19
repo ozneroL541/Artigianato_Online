6629561887ad4abea9937ed1661c0a77c4a31e1e
leonardobasso <parliamodiarte1@gmail.com>
2025-05-19 15:12:45 +0200

    Logout implemented, removed useless getters (just use the default getter for localstorage)

--- src/frontend/public/script/state.js
+++ src/frontend/public/script/state.js
@@ -10,19 +10,3 @@ export function setUserInfo(jwt, userType) {
     localStorage.setItem('userType', userType)
     localStorage.setItem('isUserLogged', "true")
 }
-
-/**
- * This function returns an array with the user infos
- * @return {object} an object with the following parameters: <br>
- * `userToken` for the JWT Token <br>
- * `userType` for the user Type, which should always be artisan or client <br>
- * `isUserLogged` for checking if the user is logged or not
- * @author Leonardo Basso
- * */
-export function getUserInfo() {
-    return {
-        userToken: localStorage.getItem("userToken"),
-        userType: localStorage.getItem("UserType"),
-        isUserLogged: localStorage.getItem("isUserLogged")
-    }
-}
