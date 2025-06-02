423f1d5f0be7e6e020c39906a82a318ec145e442
Sballerini3 <ballerinistefano81@gmail.com>
2025-06-02 23:25:37 +0200

    Create gestioneprofilo.js

---
+++ src/backend/profileClient/gestioneprofilo.js
@@ -0,0 +1,69 @@
+import { ProfileClient } from './ProfileClient.js';
+ import {Segnala} from './Segnalachion.js';
+
+/**
+ * Reset the password for a given user.
+ * @param {Object} req - The request object containing { username, newPassword }
+ * @param {Object} res - The response object to send the result
+ */
+export const resetPassword = async (req, res) => {
+    const { username, newPassword } = req.body;
+    try {
+        const profile = new ProfileClient(username);
+        const result = await profile.resetPassword(newPassword);
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Bad request', error: error.message });
+    }
+}
+
+/**
+ * Reset the email for a given user.
+ * @param {Object} req - The request object containing { username, newEmail }
+ * @param {Object} res - The response object to send the result
+ */
+export const resetMail = async (req, res) => {
+    const { username, newEmail } = req.body;
+    try {
+        const profile = new ProfileClient(username);
+        const result = await profile.resetMail(newEmail);
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Bad request', error: error.message });
+    }
+}
+
+/**
+ * Reset the email for a given user.
+ * @param {Object} req - The request object {idSignal, orderId, description, resolved}
+ * @param {Object} res - The response object to send the result
+ */
+export const Segnalachion = async (req, res) => {
+    const { idSignal, orderId, description, resolved } = req.body;
+    try {
+        const sen = new Segnala(idSignal, orderId,description,resolved);
+        const result = await sen.newSignal();
+        res.status(200).json(result);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Bad request', error: error.message });
+    }
+}
+
+/**
+ * Reset the email for a given user.
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object to send the result
+ */
+export const GetBuyproduct=async (req, res) => {
+    try {
+        const profile = new ProfileClient(req.query.username); 
+        const products = await profile.getBuyProducts();
+        res.status(200).json(products);
+    } catch (error) {
+        console.error(error);
+        res.status(400).json({ message: 'Bad request', error: error.message });
+    }
+}
