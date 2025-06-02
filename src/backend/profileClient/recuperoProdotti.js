99f2c55c4e42ba6867d1ac9e6bb93a70e36ae3f7
Sballerini3 <ballerinistefano81@gmail.com>
2025-06-02 23:37:25 +0200

    Update recuperoProdotti.js

--- src/backend/profileClient/recuperoProdotti.js
+++ src/backend/profileClient/recuperoProdotti.js
@@ -1,4 +1,4 @@
-import { ProductResearch } from './backend/dashboard/searchProductClass.js';
+import { ProductResearch } from './src/backend/dashboard/searchProductClass.js';
 
 /**
  * Research of a product by ID.
