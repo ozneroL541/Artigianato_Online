650712f61e977a3874ae82bae609d44ef56e7609
leonardobasso <parliamodiarte1@gmail.com>
2025-06-08 18:16:58 +0200

    written documentation

---
+++ src/frontend/public/script/crud/admin/register.js
@@ -0,0 +1,32 @@
+
+/**
+ * This function implements the API for the client registration
+ * @param {string} username the username of the client
+ * @param {string} password The user password
+ * @param {string} name The client name
+ * @param {string} surname The client surname
+ * @param {string} email The client email
+ * @author Leonardo Basso
+ */
+export const registerAdmin = async (username, password) => {
+    try {
+        const response = await fetch('http://localhost:8080/api/auth/register/admin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({username, password}),
+        });
+
+        const data = await response.json();
+        if (response.ok) {
+            alert("Registrazione completata con successo!");
+            window.location.href = '/auth/admin/login';
+        } else {
+            alert(data.message);
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        alert('Si è verificato un errore durante la registrazione.');
+    }
+};
