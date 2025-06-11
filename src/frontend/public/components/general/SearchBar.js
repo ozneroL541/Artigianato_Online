1bf8e440e0bb7c784e05b2c93616448e82b60344
leonardo <parliamodiarte1@gmail.com>
2025-06-11 22:10:07 +0200

    Implemented search api for products  (not graphically), probably completed SearchBar component

--- src/frontend/public/components/general/SearchBar.js
+++ src/frontend/public/components/general/SearchBar.js
@@ -1,9 +1,5 @@
-import { getCategories, populateSelectMenu } from "../../script/utils.js";
+import {getCategories, populateSelectMenu} from "../../script/utils.js";
 
-/**
- * This component is used to display a searchbar and perform search operations
- * @author Leonardo Basso
- */
 class SearchBar extends HTMLElement {
     constructor() {
         super();
@@ -14,8 +10,8 @@ class SearchBar extends HTMLElement {
         this.shadowRoot.innerHTML = `
             <label class="homepage__product__search">
                 <span class="homepage__product__search__top">
-                    <input type="text" class="homepage__product__search__name" placeholder="Product Name">
-                    <button class="homepage__product__search__button">
+                    <input type="text" class="homepage__product__search__input" placeholder="Product Name" id="searchProductName">
+                    <button class="homepage__product__search__button" id="searchProductButton">
                         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
                             class="bi bi-search" viewBox="0 0 16 16">
                             <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001q.044.06.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1 1 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0"/>
@@ -23,9 +19,12 @@ class SearchBar extends HTMLElement {
                     </button>
                 </span>
                 <span class="homepage__product__search__bottom">
-                    <select id="searchProductCategory" class="homepage__product__search__category"></select>
-                    <input type="number" class="homepage__product__search__name" placeholder="Price">
-                    <input type="number" class="homepage__product__search__name" placeholder="in stock">
+                    <select id="searchProductCategory" class="homepage__product__search__category" >
+                        <option value="">All Categories</option>
+                    </select>
+                    <input id="searchProductMinPrice" type="number" class="homepage__product__search__input" placeholder="Min Price">
+                    <input id="searchProductMaxPrice" type="number" class="homepage__product__search__input" placeholder="Max Price">
+                    <input id="searchProductStock" type="number" class="homepage__product__search__input" placeholder="Stock">
                 </span>
             </label>
 
@@ -42,57 +41,76 @@ class SearchBar extends HTMLElement {
                     border-radius: .5rem;
                     color: #eee;
                 }
-                option {
-                    background-color: #232529;
-                }
                 .homepage__product__search {
                     display: grid;
                     width: 90vw;
-                    max-width: 1000px;
+                    max-width: 1200px;
                     gap: .3rem;
                     margin: .5rem auto;
                 }
-
                 .homepage__product__search__top,
                 .homepage__product__search__bottom {
                     display: flex;
                     gap: .3rem;
                     width: 100%;
                 }
-
-                .homepage__product__search__name,
+                .homepage__product__search__input,
                 .homepage__product__search__category {
                     width: 100%;
                     min-height: 40px;
                 }
-
                 .homepage__product__search__bottom {
-                    display: none;
+                    gap: .3rem;
                     max-height: 0;
                     opacity: 0;
                     overflow: hidden;
-                    transition: all 0.3s ease-out;
+                    transform: translateY(-10px);
+                    transition: max-height 1s ease-in-out, opacity 0.4s ease, transform 0.4s ease;
+                }
+                
+                .homepage__product__search__bottom.visible {
+                    max-height: 100vh;
+                    opacity: 1;
+                    transform: translateY(0);
                 }
 
-                .visible {
-                    display: flex;
-                    max-height: 200px;
-                    opacity: 1;
-                }
             </style>
         `;
 
-        // Shows at click the hidden search parameters
-        const searchNameInput = this.shadowRoot.querySelector('.homepage__product__search__top .homepage__product__search__name');
+        const searchNameInput = this.shadowRoot.getElementById('searchProductName');
+        const searchButton = this.shadowRoot.querySelector('.homepage__product__search__button');
         const searchBottom = this.shadowRoot.querySelector('.homepage__product__search__bottom');
         const selectProductCategory = this.shadowRoot.getElementById('searchProductCategory');
 
-        searchNameInput.addEventListener('click',() =>{
+        // Shows the full searchbar at click
+        const handleFocus = () => {
             searchBottom.classList.add('visible');
-        })
+            searchNameInput.removeEventListener('focus', handleFocus);
+        };
+        searchNameInput.addEventListener('focus', handleFocus);
 
+        // Populates the category menu
         const categories = await getCategories();
         populateSelectMenu(categories.categories, selectProductCategory);
+
+        // Send search data to /search
+        searchButton.addEventListener('click', () => {
+            const productName = this.shadowRoot.getElementById('searchProductName').value;
+            const category = selectProductCategory.value;
+            const minPrice = this.shadowRoot.getElementById('searchProductMinPrice').value;
+            const maxPrice = this.shadowRoot.getElementById('searchProductMaxPrice').value;
+            const availability = this.shadowRoot.getElementById('searchProductStock').value;
+
+            const searchParams = new URLSearchParams();
+            if (productName) searchParams.set('nome_prodotto', productName);
+            if (category) searchParams.set('categoria', category);
+            if (minPrice) searchParams.set('prezzo_min', minPrice);
+            if (maxPrice) searchParams.set('prezzo_max', maxPrice);
+            if (availability) searchParams.set('disponibilita', availability);
+
+            window.location.href = `/search?${searchParams.toString()}`;
+
+        });
     }
 }
 
---
+++ src/frontend/public/script/crud/homepage/search.js
@@ -0,0 +1,25 @@
+/**
+ * This function returns a list of products given specific params which are `name`, `category`, `minimum price`,
+ * `maximum price` and `stock`
+ * @param {URLSearchParams} params The params used to search the data
+ * @returns {Array} A list of products
+ * @author Leonardo Basso
+ */
+export const searchProduct = async (params) => {
+    try {
+        const res = await fetch(`http://localhost:8080/api/product/search?${params.toString()}`);
+
+        if (res.ok) {
+            const products = await res.json();
+            console.log(products)
+            return products;
+        } else {
+            const err = await res.json();
+            alert(`Error: ${err}`)
+            console.log(err)
+        }
+
+    } catch (err) {
+        alert(`Error: ${err}`)
+    }
+}
