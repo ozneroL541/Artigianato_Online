0dbdc0952ff34b20367f1fdaea456d84b80b2e06
leonardo <parliamodiarte1@gmail.com>
2025-06-20 20:52:10 +0200

    Small fixes

--- src/frontend/public/components/layout/PageLayout.js
+++ src/frontend/public/components/layout/PageLayout.js
@@ -55,7 +55,7 @@ class PageLayout extends HTMLElement {
                     <a class="link" href="/">Home</a>
                     ${userType === "amministratore" || userType === "artigiano" || userType === "cliente" ? '<a class="link" href="/' + userType + '/dashboard">Dashboard</a>' : ''}
                     ${userType !== "unregistered" ? '<a class="link" id="logoutLink" href="#">Logout</a>' : '<a class="link" href="/auth/login">Login</a>'}
-                    ${userType === "cliente" ? `<a id="toggleProductCart" class="link" href="/user/product-cart"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16">
+                    ${userType === "cliente" ? `<a id="toggleProductCart" class="link" href="/cliente/product-cart"> <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-cart-fill" viewBox="0 0 16 16">
                         <path d="M0 1.5A.5.5 0 0 1 .5 1H2a.5.5 0 0 1 .485.379L2.89 3H14.5a.5.5 0 0 1 .491.592l-1.5 8A.5.5 0 0 1 13 12H4a.5.5 0 0 1-.491-.408L2.01 3.607 1.61 2H.5a.5.5 0 0 1-.5-.5M5 12a2 2 0 1 0 0 4 2 2 0 0 0 0-4m7 0a2 2 0 1 0 0 4 2 2 0 0 0 0-4m-7 1a1 1 0 1 1 0 2 1 1 0 0 1 0-2m7 0a1 1 0 1 1 0 2 1 1 0 0 1 0-2"/>
                         </svg> </a>` : ''}
                 </nav>
--- src/frontend/public/script/utils.js
+++ src/frontend/public/script/utils.js
@@ -95,5 +95,5 @@ export const formatDate = (timestamp) => {
     const day = String(dateObj.getDate()).padStart(2, '0');
     const hours = String(dateObj.getHours()).padStart(2, '0');
     const minutes = String(dateObj.getMinutes()).padStart(2, '0');
-    return `${year}/${month}/${day} ${hours}:${minutes}`;
+    return `${day}/${month}/${year} ${hours}:${minutes}`;
 }
